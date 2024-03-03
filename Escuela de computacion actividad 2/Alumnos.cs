43ee08c301e38552802580483db92e8a334f8643
Hermanos <Hermanos@DESKTOP-NLNVGLJ>
2024-03-03 09:30:41 -0600

    Agregar archivos de proyecto.

---
+++ Escuela de computacion actividad 2/Alumnos.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Escuela_de_computacion_actividad_2
+{
+    internal class Alumnos
+    {
+    }
+}
